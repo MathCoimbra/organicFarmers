ec30284731c4057c1965307079b50d7a428775d2
MathCoimbra <matheuscoimbra35@gmail.com>
2022-09-02 21:29:49 -0300

    adicionada tela parcial de carrinho de compras

---
+++ app/src/main/java/br/com/fiap/organicfarmers/CarrinhoActivity.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.organicfarmers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CarrinhoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carrinho)
+    }
+}
