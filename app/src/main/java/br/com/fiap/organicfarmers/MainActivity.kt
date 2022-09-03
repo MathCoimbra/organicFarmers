e4b546d90be552aa1e040454d1de049e5f2415ef
Rafael Vinicius <viniciosampaio@hotmail.com>
2022-09-03 17:17:06 -0300

    feat: finish main activity

--- app/src/main/java/br/com/fiap/organicfarmers/MainActivity.kt
+++ app/src/main/java/br/com/fiap/organicfarmers/MainActivity.kt
@@ -2,10 +2,18 @@ package br.com.fiap.organicfarmers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        imageView.setImageResource(R.drawable.union)
+
+
     }
 }
